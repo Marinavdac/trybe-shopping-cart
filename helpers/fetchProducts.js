47e8c177e4ed82d476373e48161d64d2281042ff
Marina Andrade Catão <marinavdac@gmail.com>
2022-05-01 20:29:18 -0300

    feat: adiciona resolucao final do item 08 do projeto

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,16 +1,11 @@
 const fetchProducts = async (query) => {
   const url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
-   
   try {
     const results = await fetch(url);
     const data = await results.json();
-  
-    if (!query) {
-      throw new Error('You must provide an url');
-    }
     return data;
   } catch (error) {
-    console.log(`Algo deu errado :( \n${error.message}`);
+    return error;
   }
 };
 
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -21,7 +21,13 @@ describe('1 - Teste a função fecthProducts', () => {
   test('Verifica se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch', async() => {
     expect.assertions(1);
     const data = await fetchProducts('computador');
-    expect(data).toEqual(computadorSearch);
+    console.log('data', data);
+    expect(data).toMatchObject(computadorSearch);
+  });
+  test('Verifica se o retorno da função fetchProducts com o argumento "camisa" é uma estrutura de dados distinta do objeto computadorSearch', async() => {
+    expect.assertions(1);
+    const data = await fetchProducts('camisa');
+    expect(data).not.toEqual(computadorSearch);
  });
  test('Verifica se ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url', async () => {
   try {
